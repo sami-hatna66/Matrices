92f58ddd3b0b4562c14ac9ddc9df2f49bc451092
Sami Hatna <sami.hatna2003@gmail.com>
2022-08-28 18:08:22 +0100

    Initial Commit

---
+++ Matrix/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Matrix
+//
+//  Created by Sami Hatna on 28/08/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
